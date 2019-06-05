8c6093efc488f760e312a24f0a129d8f4c583bbb
Matthew Jenkins <matthew.jenkins@waystar.com>
2019-06-05 09:59:21 -0400

    Add project files.

---
+++ MySite.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MySite.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
